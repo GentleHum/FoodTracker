c1e0d6e9c132c633a7afb4c520390ea87f38ce6f
Mike V <mwvork@gmail.com>
2018-05-25 17:15:33 -0400

    Added images for various categories on the FoodItemDetail screen.

--- FoodTracker/Controllers/FoodItemDetailViewController.swift
+++ FoodTracker/Controllers/FoodItemDetailViewController.swift
@@ -14,6 +14,30 @@ final class FoodItemDetailViewController: UIViewController {
         static let title = "RHN"
     }
     
+    private static let defaultCategoryImageName = "vegetables.jpg"
+    private static let categoryImages: [FoodCategory: String] = [
+        .meat : "meats.jpg",
+        .vegetable : "vegetables.jpg",
+        .grain : "grains.jpg",
+        .nut : "nuts.jpg",
+        .fruit : "fruits.jpg",
+        .beverage: "beverages.jpg",
+        .dairy : "dairy.jpg",
+        .alcohol : "alcohol.jpg",
+        .baking : "baking.jpg",
+        .condiment : "condiments.jpg",
+        .fat : "fats.jpg",
+        .herbSpice : "herbs.jpg",
+        .supplement : "supplements.jpg",
+        .sweetener : "sweeteners.jpg",
+    ]
+    
+    private static func getCategoryImage(forCategory category: FoodCategory) -> UIImage? {
+        let imageName = categoryImages[category] ?? defaultCategoryImageName
+        return UIImage(named: imageName)
+    }
+    
+    // MARK: - Outlets
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var oxalateValueLabel: UILabel!
@@ -23,6 +47,8 @@ final class FoodItemDetailViewController: UIViewController {
     @IBOutlet weak var scdValueLabel: UILabel!
     @IBOutlet weak var paleoValueLabel: UILabel!
     @IBOutlet weak var fodmapValueLabel: UILabel!
+    @IBOutlet weak var categoryImageView: UIImageView!
+
 
 
     override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +78,8 @@ extension FoodItemDetailViewController: StoreSubscriber {
     func newState(state: FoodsState) {
         print("+++++++++++FoodItemDetailViewController: newState")  // zap
         nameLabel.text = state.selectedItem.name
+        categoryImageView.image =
+            FoodItemDetailViewController.getCategoryImage(forCategory: state.selectedItem.category)
         categoryLabel.text = state.selectedItem.category.description()
         oxalateValueLabel.text = "Oxalate: " + state.selectedItem.oxalateContent.description()
         salicylateValueLabel.text = "Salicylate: " + state.selectedItem.salicylateContent.description()
